64c0def5f31b64cca39a7052cf9fd7ce796bb661
adj-dev <andrew.johnson2073@gmail.com>
2019-06-04 20:39:15 -0500

    made minor change

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 
 
 // Configure connection settings
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
